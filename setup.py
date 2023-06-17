e7a00a6444a0f4f3c3c729c1754da85aff4540d3
Supreeth Rao <55043035+SupreethRao99@users.noreply.github.com>
2023-06-18 00:55:27 +0530

    Update setup.py to enable automatic version numbers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ai_incantations",
-    version="0.0.1",
+    version="{{VERSION_PLACEGOLDER}}",
     author="Supreeth Rao",
     description="A library to manage and develop prompts",
     packages=["ai_incantations"],
