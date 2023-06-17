811126c27daf2a508c9363e372c1efe7cc9c22ec
Supreeth Rao <55043035+SupreethRao99@users.noreply.github.com>
2023-06-18 01:07:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ai_incantations",
-    version="{{VERSION_PLACEGOLDER}}",
+    version="{{VERSION_PLACEHOLDER}}",
     author="Supreeth Rao",
     description="A library to manage and develop prompts",
     packages=["ai_incantations"],
